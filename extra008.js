bdf7d6a1747d1d0f4e0bf14328e9c1d2fda24458
Diego Reis <diego_reis7@hotmail.com>
2022-10-14 22:21:18 -0300

    exercicios
    Acrescentado os exercícios que foram feitos

---
+++ extra008.js
@@ -0,0 +1,54 @@
+
+    // var teste = 1
+    // alert(teste)
+    // var teste = 2 // isso é possivel com o método VAR
+    // alert(teste)
+
+    // let oshi = 3
+
+    //  // Nesse caso é permitido 
+    //  // redeclarar a variavel LET, 
+    //  // pois estão em diferentes blocos de execução
+
+    // {                  
+    //     let oshi = 4 
+    // }
+
+    // const constante = 9
+    
+    // alert(constante) 
+
+    // // como no LET permite que a variavel seja redeclarada apenas se estiverem em blocos de execução diferentes
+
+    // {
+    //     const constante = 10
+    //     alert(constante)  
+    // }
+
+    // var teste = 2
+    // teste = 4  // com var isso é totalmente possivel
+    // teste = 6 // var era 2 virou 4 e agora é 6
+    // alert("O Teste é igual a " + teste)
+
+    // let wi = 58 // com let também é possivel atribuir novos valores a uma variavel
+    // wi = 70 // wi era 58 agora passa a ser 70
+    // alert(wi)
+
+    // var x = 2;
+    // var y = 7;
+    // var z = x + y;
+    // alert("O Valor de Z é " + z);
+    // // quando ambos os valores são numéricos o Javascript compreende que é para ser feito uma soma entre os valores, porém caso haja uma string entre esses valores, ou se caso por padrão o valor que você tenha usado, use string como tipo de dado padrão ao invés de executar o calculo matemático, ele vai concatenar os dois valores.
+    // var a = "a";
+    // var b = 7;
+    // var c = a + b ;
+    // alert("O Valor de C é " + c);
+
+    var a = 4
+    a--
+    alert(a)
+
+    //  ( )   primeiro
+    //  **  segundo
+    //  * /   terceiro
+   //   + -  quarto
